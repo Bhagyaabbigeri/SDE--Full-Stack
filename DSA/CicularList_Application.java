7c60dbe002866c68ae03d1e60240ada88116d282
Bhagyaabbigeri <your-email@example.com>
2025-10-17 21:36:53 +0530

    JAVA

---
+++ DSA/CicularList_Application.java
@@ -0,0 +1,9 @@
+public class CicularList_Application {
+
+    public static void main(String[] args) {
+        CircularList circularList = new CircularList();
+
+        circularList.testCicularList();
+    }
+
+}
